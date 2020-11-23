e282206df816a71b8369bda2e70038947abc99f1
TomozQ <63640328+TomozQ@users.noreply.github.com>
2020-11-23 11:21:40 +0900

    jsAppendText

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("addText").value;
+  document.getElementById("addText").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "listRow";
+
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完成リストに追加
+  document.getElementById("imcompleteList").appendChild(div);
+};
+
+document
+  .getElementById("addButton")
+  .addEventListener("click", () => onClickAdd());
